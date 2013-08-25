c7ffd88890c538abac73202cb87f25bc3a4c91fa
Bibek Shrestha <bibekshrestha@gmail.com>
2013-08-25 10:42:07 +0200

    Fixed testcase, removed print_r

--- tests/phpwhois/WhoisClientTest.php
+++ tests/phpwhois/WhoisClientTest.php
@@ -8,4 +8,19 @@ class WhoisClientTest extends PHPUnit_Framework_TestCase {
     $result = $whois->FetchRawData($query_params);
     print_r($result);
   }
+
+  function testWhoisQueryParams() {
+    $domain = "google.ws";
+    $whois = new PhpwhoisQuery();
+    $query_params = $whois->WhoisQueryParams($domain);
+    // print_r($query_params);
+  }
+
+  function testFetchRawData() {
+    $domain = "google.ws";
+    $whois = new PhpwhoisQuery();
+    $data = $whois->WhoisQueryParams($domain);
+    $result = $whois->FetchRawData($data);
+    // print_r($data); 
+  }
 }
